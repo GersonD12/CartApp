90af213c4033211e84cab19573377162e9025ea4
Gerson Perez Menchu <gersonperez715@gmail.com>
2024-11-19 12:16:29 -0600

    Ajuste de rutas

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import '../css/Footer.css'
 export const Footer = () => {
   return (
     <footer>
-      <p>© 2024 Mi Sitio Web. Comentarios Gerson.</p>
+      <p>© 2024 CartAppBasic. Comentarios Gerson.</p>
     </footer>
   );
 }
